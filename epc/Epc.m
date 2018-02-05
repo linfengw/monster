9cb71382383bcb6ec523f1e2684ffbe5124611d5
Matteo <matart@fotonik.dtu.dk>
2018-02-05 08:31:23 +0100

    Initial commit

---
+++ epc/Epc.m
@@ -0,0 +1,17 @@
+%   EPC defines a value class for the core network
+
+classdef Epc
+	%   EPC 
+	properties
+		
+	end
+	
+	methods
+		% Constructor
+		function obj = Epc(Param)
+		end
+		
+				
+	end
+	
+end
